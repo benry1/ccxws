23f39bd3eb344247f1015d27596f9371c544fb0c
benry1 <90576401+benry1@users.noreply.github.com>
2022-04-09 14:31:51 -0400

    Update api url

--- src/exchanges/ZbClient.ts
+++ src/exchanges/ZbClient.ts
@@ -14,7 +14,7 @@ import { Trade } from "../Trade";
 export class ZbClient extends BasicClient {
     public remoteIdMap: Map<string, string>;
 
-    constructor({ wssPath = "wss://api.zb.work/websocket", watcherMs }: ClientOptions = {}) {
+    constructor({ wssPath = "wss://live.zb.work/websocket", watcherMs }: ClientOptions = {}) {
         super(wssPath, "ZB", undefined, watcherMs);
         this.hasTickers = true;
         this.hasTrades = true;
